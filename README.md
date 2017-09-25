6f33ef633317f75e2c30c734efa1bf8788624504
Benjamin Rozière <benjamin.roziere@rdalambic.com>
2017-09-25 18:12:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+zzAgenda
+========
+
+A Symfony project created on September 25, 2017, 4:05 pm.
---
+++ var/cache/dev/twig/ba/ba798dd52e9f3684ad1200a8287303ec393739df49c3cddba132c0c8d013dc06.php
@@ -0,0 +1,66 @@
+<?php
+
+/* @WebProfiler/Icon/close.svg */
+class __TwigTemplate_eee66ec0767492bb0ec742d354025edad3d31dfbca46595f4864b8e724be5be3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_2f67029926757e24204e71662ce39bb7501980e59499330ea8a2395cd904c125 = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_2f67029926757e24204e71662ce39bb7501980e59499330ea8a2395cd904c125->enter($__internal_2f67029926757e24204e71662ce39bb7501980e59499330ea8a2395cd904c125_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Icon/close.svg"));
+
+        $__internal_61b797b0859fff55296c0eb055057d4cde8a857174cf1bdb8784815eb967d5e4 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_61b797b0859fff55296c0eb055057d4cde8a857174cf1bdb8784815eb967d5e4->enter($__internal_61b797b0859fff55296c0eb055057d4cde8a857174cf1bdb8784815eb967d5e4_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "@WebProfiler/Icon/close.svg"));
+
+        // line 1
+        echo "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+<path fill=\"#AAAAAA\" d=\"M21.1,18.3c0.8,0.8,0.8,2,0,2.8c-0.4,0.4-0.9,0.6-1.4,0.6s-1-0.2-1.4-0.6L12,14.8l-6.3,6.3
+    c-0.4,0.4-0.9,0.6-1.4,0.6s-1-0.2-1.4-0.6c-0.8-0.8-0.8-2,0-2.8L9.2,12L2.9,5.7c-0.8-0.8-0.8-2,0-2.8c0.8-0.8,2-0.8,2.8,0L12,9.2
+    l6.3-6.3c0.8-0.8,2-0.8,2.8,0c0.8,0.8,0.8,2,0,2.8L14.8,12L21.1,18.3z\"/>
+</svg>
+";
+        
+        $__internal_2f67029926757e24204e71662ce39bb7501980e59499330ea8a2395cd904c125->leave($__internal_2f67029926757e24204e71662ce39bb7501980e59499330ea8a2395cd904c125_prof);
+
+        
+        $__internal_61b797b0859fff55296c0eb055057d4cde8a857174cf1bdb8784815eb967d5e4->leave($__internal_61b797b0859fff55296c0eb055057d4cde8a857174cf1bdb8784815eb967d5e4_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@WebProfiler/Icon/close.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  25 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" enable-background=\"new 0 0 24 24\" xml:space=\"preserve\">
+<path fill=\"#AAAAAA\" d=\"M21.1,18.3c0.8,0.8,0.8,2,0,2.8c-0.4,0.4-0.9,0.6-1.4,0.6s-1-0.2-1.4-0.6L12,14.8l-6.3,6.3
+    c-0.4,0.4-0.9,0.6-1.4,0.6s-1-0.2-1.4-0.6c-0.8-0.8-0.8-2,0-2.8L9.2,12L2.9,5.7c-0.8-0.8-0.8-2,0-2.8c0.8-0.8,2-0.8,2.8,0L12,9.2
+    l6.3-6.3c0.8-0.8,2-0.8,2.8,0c0.8,0.8,0.8,2,0,2.8L14.8,12L21.1,18.3z\"/>
+</svg>
+", "@WebProfiler/Icon/close.svg", "/Users/benjamin/ISIMA/2A/CIWEB/zzAgenda/vendor/symfony/symfony/src/Symfony/Bundle/WebProfilerBundle/Resources/views/Icon/close.svg");
+    }
+}
