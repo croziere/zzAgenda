023268789ba401f4503da843ad8658b49894e56a
Ex-Ark <the-gameur-63@hotmail.fr>
2017-11-13 19:29:51 +0100

    added all template for events

--- module/EventModule/Controller/EventController.php
+++ module/EventModule/Controller/EventController.php
@@ -17,10 +17,20 @@ class EventController extends Controller
     public function getEventsAction(){
         $database = $this->container->get('database');
         $table = $database->getTable('event');
-        $templating = $this->container->get('templating');
 
+        $data_set = $table->select("*");
+        $events_array = $data_set->fetch();
+        
+        return $this->render('allEvents.html.twig', array(
+            'event_array' => $events_array,
+            'title' => 'Tous les evenements'
+        ));
+    }
 
+    public function getEventAction(){
+        // TODO : use getEvents , filter only needed event
         return $this->render('oneEvent.html.twig', array(
+            'title' => 'Voir evenement',
             'eventday' => 8,
             'eventmonth' => 'Juin',
             'eventyear' => 2017,
